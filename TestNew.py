195cee88a011a12bbd65e48d8d64e69c95189317
Jovan Stojkovic <t-jstojkovic@microsoft.com>
2023-06-30 13:31:41 -0700

    msg

--- TestNew.py
+++ TestNew.py
@@ -61,7 +61,7 @@ def EnforceActivityWindow(start_time, end_time, instance_events):
     return events_iit
 
 
-def lambda_call(lastId, queue):
+def lambda_call_sgraph(queueL):
     t1 = time.time()
     address = addresses["socialnetwork_social-graph-service_1"]
     socket = TSocket.TSocket(address, 9090)
@@ -80,22 +80,33 @@ def lambda_call(lastId, queue):
 
     transport.close()
     t2 = time.time()
-    queue.put(t2 - t1)
+    queueL.put(t2 - t1)
     return 0
 
 
-def lambda_proxy(lastId, queue):
-    sum = 0
-    indX = 0
-    while True:
-        sum += indX
-        indX += 1
+def lambda_call_user(queueL):
+    t1 = time.time()
+    address = addresses["socialnetwork_user-service_1"]
+    socket = TSocket.TSocket(address, 9090)
+    transport = TTransport.TFramedTransport(socket)
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    client = SocialGraphService.Client(protocol)
 
+    user = random.randint(1, 80000)
+
+    transport.open()
+    req_id = uuid.uuid4().int & (1 << 32)
+    client.Login(req_id, "username_" + str(user), "password_" + str(user), {})
+
+    transport.close()
+    t2 = time.time()
+    queueL.put(t2 - t1)
+    return 0
 
 duration = 10
 seed = 100
 rates = [20, 50, 80, 100, 120, 150, 180, 200, 220, 250, 280, 300, 320, 350, 380, 400, 420, 450, 480, 500]
-rates = [10]
+rates = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
 # generate Poisson's distribution of events
 instance_events_list = []
 for rate in rates:
@@ -105,36 +116,35 @@ for rate in rates:
     inter_arrivals = list(np.random.exponential(scale=beta, size=int(oversampling_factor * duration * rate)))
     instance_events_list.append(EnforceActivityWindow(0, duration, inter_arrivals))
 
-queue = multiprocessing.Queue()
-for instance_events in instance_events_list:
-    print(instance_events_list.index(instance_events))
-    # time.sleep(15)
-    after_time, before_time = 0, 0
-    st = 0
-    tids = []
-    times = []
-    indT = 40000
-    for t in instance_events:
-        st = st + t - (after_time - before_time)
-        before_time = time.time()
-        if st > 0:
-            time.sleep(st)
-        thread = threading.Thread(target=lambda_call, args=(indT, queue,))
-        thread.start()
-        tids.append(thread)
-        after_time = time.time()
-        indT += 1
+for repetition in range(0, 3):
+    for instance_events in instance_events_list:
+        queue = multiprocessing.Queue()
+        print(instance_events_list.index(instance_events))
+        time.sleep(10)
+        after_time, before_time = 0, 0
+        st = 0
+        tids = []
+        times = []
+        for t in instance_events:
+            st = st + t - (after_time - before_time)
+            before_time = time.time()
+            if st > 0:
+                time.sleep(st)
+            thread = threading.Thread(target=lambda_call_user, args=(queue,))
+            thread.start()
+            tids.append(thread)
+            after_time = time.time()
 
-    done = 0
-    for tid in tids:
-        done += 1
-        if done % 1000 == 0:
-            print("Done = ", done)
-        tid.join(5)
+        done = 0
+        for tid in tids:
+            done += 1
+            if done % 1000 == 0:
+                print("Done = ", done)
+            tid.join(5)
 
-    for tid in tids:
-        times.append(queue.get())
+        for tid in tids:
+            times.append(queue.get())
 
-    print("P50 = ", round(1000 * np.percentile(times, 50), 2), "ms")
-    print("P90 = ", round(1000 * np.percentile(times, 90), 2), "ms")
-    print("P99 = ", round(1000 * np.percentile(times, 99), 2), "ms")
+        print("P50 = ", round(1000 * np.percentile(times, 50), 2), "ms")
+        print("P90 = ", round(1000 * np.percentile(times, 90), 2), "ms")
+        print("P99 = ", round(1000 * np.percentile(times, 99), 2), "ms")
