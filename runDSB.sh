e9910d91180663e2ac2de452e5d2823af4aac7d4
Jovan Stojkovic <t-jstojkovic@microsoft.com>
2023-07-04 15:25:12 -0700

    add new

--- runDSB.sh
+++ runDSB.sh
@@ -17,6 +17,26 @@ sudo usermod -aG docker $USER
 
 sudo chmod 666 /var/run/docker.sock
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo apt-get install python3-pip -y
 pip3 install docker
-pip3 install numpy==1.19.5
+pip3 install numpy
+
+git clone https://github.com/jovans2/DeathStarBench_TestScripts
+git clone https://github.com/delimitrou/DeathStarBench
+
+cp DeathStarBench_TestScripts/docker-compose.yml DeathStarBench/socialNetwork/
+cp DeathStarBench_TestScripts/service-config.json DeathStarBench/socialNetwork/config/
+cp DeathStarBench_TestScripts/FrontendSimple.py DeathStarBench/socialNetwork/test
+
+docker pull jovanvr97/deathstarbench
+
+cd DeathStarBench/socialNetwork
+
+docker-compose up -d
+
+cd test
+
+python3 FrontendSimple.py
