1b4f7d3ebd4e89ed93c42cc9c9b01981c2953923
Jovan Stojkovic <t-jstojkovic@microsoft.com>
2023-07-12 10:13:52 -0700

    add event listener

--- FrontendSimple.py
+++ FrontendSimple.py
@@ -11,6 +11,7 @@ import docker
 import multiprocessing
 import time
 import numpy as np
+import requests
 
 from social_network import SocialGraphService
 from social_network import UserService
@@ -30,10 +31,6 @@ from opencensus.tags import tag_value as tag_value_module
 from datetime import datetime
 from opencensus.ext.azure import metrics_exporter
 
-services = ["socialnetwork_social-graph-service_1", "socialnetwork_social-graph-service-first_1", "socialnetwork_social-graph-service-second_1",
-            "socialnetwork_social-graph-service-third_1", "socialnetwork_social-graph-service-fourth_1", "socialnetwork_social-graph-service-fifth_1",
-            "socialnetwork_social-graph-service-sixth_1", "socialnetwork_social-graph-service-seventh_1", "socialnetwork_social-graph-service-eight_1",
-            "socialnetwork_social-graph-service-ninth_1"]
 services = ["socialnetwork_user-service_1"]
 
 addresses = {}
@@ -59,29 +56,19 @@ tmap = tag_map_module.TagMap()
 mmap1 = stats_recorder.new_measurement_map()
 tmap1 = tag_map_module.TagMap()
 
-# Create the tag key
-# key_method = tag_key_module.TagKey("method")
-# Create the status key
-# key_status = tag_key_module.TagKey("status")
-# Create the error key
-# key_error = tag_key_module.TagKey("error")
-
 latency_view = view_module.View("latency_"+str(os.environ['WORKLOAD_ID']), "The distribution of the latencies",
-    #[key_method, key_status, key_error],
-    [],
-    m_latency_ms,
-    # Latency in buckets:
-    # [>=0ms, >=25ms, >=50ms, >=75ms, >=100ms, >=200ms, >=400ms, >=600ms, >=800, >=1000]
-    #aggregation_module.DistributionAggregation([0, 25, 50, 75, 100, 200, 400, 600, 800, 1000]))
-    aggregation_module.LastValueAggregation())
+                                [],
+                                m_latency_ms,
+                                aggregation_module.LastValueAggregation())
 
-# exporter = metrics_exporter.new_metrics_exporter(connection_string='InstrumentationKey=080046f1-79d3-48ce-8abc-1d58acb0504a;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/')
 exporter = metrics_exporter.new_metrics_exporter(connection_string=os.environ['APPLICATIONINSIGHTS_CONNECTION_STRING'])
 
 view_manager.register_view(latency_view)
 view_manager.register_exporter(exporter)
 
 output_file = open("/tmp/tailLog.log", "a")
+
+toBeTerminated = False
 
 def lambda_call_user():
     global addresses
@@ -102,20 +89,6 @@ def lambda_call_user():
 
     transport.close()
     t2 = time.time()
-
-    end_ms = (t2 - t1) * 1000.0 # Seconds to milliseconds
-
-    # Record the latency
-    # mmap1.measure_float_put(m_latency_ms, end_ms)
-
-    # tmap1.insert(key_method, tag_value_module.TagValue("repl"))
-    # tmap1.insert(key_status, tag_value_module.TagValue("OK"))
-
-    # Insert the tag map finally
-    # mmap1.record(tmap1)
-    # metrics1 = list(mmap1.measure_to_view_map.get_metrics(datetime.utcnow()))
-    # print("Latency = ", end_ms)
-    # print(metrics1)
 
     queueTimes.put(t2-t1)
     return 0
@@ -148,7 +121,32 @@ def sendOK(clientSocket):
     except:
         clientSocket.close()
 
+def sendNotOK(clientSocket):
+    msg = 'NotOK'
+    response_headers = {
+        'Content-Type': 'text/html; encoding=utf8',
+        'Content-Length': len(msg),
+        'Connection': 'close',
+    }
+    response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
+
+    response_proto = 'HTTP/1.1'
+    response_status = '404'
+    response_status_text = 'NotOK'  # this can be random
+
+    # sending all this stuff
+    r = '%s %s %s\r\n' % (response_proto, response_status, response_status_text)
+    try:
+        clientSocket.send(r.encode(encoding="utf-8"))
+        clientSocket.send(response_headers_raw.encode(encoding="utf-8"))
+        clientSocket.send('\r\n'.encode(encoding="utf-8"))  # to separate headers from body
+        clientSocket.send(msg.encode(encoding="utf-8"))
+        clientSocket.close()
+    except:
+        clientSocket.close()
+
 def HealthThread():
+    global toBeTerminated
     myHost = '0.0.0.0'
     myPort = 3333
 
@@ -160,14 +158,14 @@ def HealthThread():
     while True:
         (clientSocket, _) = serverSocket.accept()
         clientSocket.recv(1024)
-        threading.Thread(target=sendOK, args=(clientSocket,)).start()
+        if not toBeTerminated:
+            threading.Thread(target=sendOK, args=(clientSocket,)).start()
+        else:
+            threading.Thread(target=sendNotOK, args=(clientSocket,)).start()
 
 def TailSLOThread():
     global queueTimes
 
-    SLO = 0.15
-    threshold1 = 0.7
-    threshold2 = 0.8
     while True:
         time.sleep(2)
         currTimes = []
@@ -182,25 +180,35 @@ def TailSLOThread():
         print(currentTailMs, file=output_file, flush=True)
         mmap1.measure_float_put(m_latency_ms, currentTailMs)
 
-        # tmap1.insert(key_method, tag_value_module.TagValue("repl"))
-        # tmap1.insert(key_status, tag_value_module.TagValue("OK"))
-
         # Insert the tag map finally
         mmap1.record(tmap1)
-        if currentTail > threshold2 * SLO:
-            # print("Need to scale out!")
-            mmap.measure_int_put(prompt_measure, 1)
-            mmap.record(tmap)
-            metrics = list(mmap.measure_to_view_map.get_metrics(datetime.utcnow()))
-            # print(metrics)
-            # print(metrics[0].time_series[0].points[0])
-        elif currentTail > threshold1 * SLO:
-            print("Need to scale up!")
+
+def EventCheckThread():
+    global toBeTerminated
+    while True:
+        time.sleep(5)
+        headers = {'Metadata': 'True'}
+        query_params = {'api-version': '2020-07-01'}
+        endpoint = "http://169.254.169.254/metadata/scheduledevents"
+        rsp = requests.get(endpoint, headers=headers, params=query_params).json()
+        rcvEvents = rsp["Events"]
+        if len(rcvEvents) > 0:
+            for event in rcvEvents:
+                if event["EventType"] == "Terminate":
+                    toBeTerminated = True
 
 def serveRequest(clientSocket):
-    clientSocket.recv(1024)
-    lambda_call_user()
-    sendOK(clientSocket)
+    childPid = os.fork()
+    if childPid == 0:
+        clientSocket.recv(1024)
+        lambda_call_user()
+        sendOK(clientSocket)
+        os._exit(os.EX_OK)
+    else:
+        try:
+            os.waitpid(childPid, 0)
+        except:
+            pass
     return 0
 
 def run():
@@ -226,6 +234,10 @@ def run():
     # Set the thread for Tail Latency monitoring
     threadTailSLO = threading.Thread(target=TailSLOThread)
     threadTailSLO.start()
+
+    # Set the thread for event checking
+    threadEvents = threading.Thread(target=EventCheckThread)
+    threadEvents.start()
 
     # If a request come, then fork.
     while True:
